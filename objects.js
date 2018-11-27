71433ffd0363b1694d45f80dd499d6124a1856d9
Elaina <elainaseki@gmail.com>
2018-11-27 05:36:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,9 @@
 var playlist = {
-  Slowdive:'Alison',
-  My Bloody Valentine:'Sometimes'
+  Alison :'Slowdive',
+  Sometimes :' My Bloody Valentine'
 };
 
-function updatePlaylist(playlist,'Phil Ochs','Here\'s to the State of Mississippi'){
+function updatePlaylist(playlist,artistName,songTitle){
+  playlist['Phil Ochs'] = 'Here\'s to the State of Mississippi';
 return playlist;
 }
