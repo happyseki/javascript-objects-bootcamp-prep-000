49db6c522342c485ae0857c13e4c66df34b7d41a
Elaina <elainaseki@gmail.com>
2018-11-27 05:19:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,6 @@ var playlist = {Taylor:'Last Christmas'};
 function updatePlaylist(playlist,artistName,songTitle){
 playlist.['Slowdive']='Alison';
 playlist.['My Bloody Valentine']='Sometimes';
+return playlist;
   
 }
